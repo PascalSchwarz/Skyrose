d4c29587bcb9266528df1cb38c186a92f26c18e6
Pascal Schwarz <p.schwarz1997@googlemail.com>
2022-07-31 20:33:54 +0200

    add azimut and mirror code

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 #include <DMXSerial.h>
 #include <L298N.h>
 #include "A4988.h"
+#include <EEPROM.h>
+
+//------------------------------------
+// Defines for Pins
+//------------------------------------
 
 // Pins for the stepper motor shutter
 #define MOTOR_STEPS 200
@@ -30,6 +35,19 @@
 // Onboard LED
 #define LED 13
 
+//------------------------------------
+// Defines for other stuff
+//------------------------------------
+
+#define azOffset 0
+#define miOffset 1
+#define shutOffset 2
+#define lampOffset 3
+
+//------------------------------------
+// Globals and instances of stuff
+//------------------------------------
+
 // init of stepper
 A4988 stepper(MOTOR_STEPS, DIR, STEP);
 
@@ -39,11 +57,67 @@ L298N azimut(azEN, azIN1, azIN2);
 // Create mirror motor instance
 L298N mirror(miEN, miIN1, miIN2);
 
+// dmxAddr variable, will be loaded from eeprom at startup
+uint16_t dmxAddr = 0;
+
+//------------------------------------
+// Run setup once
+//------------------------------------
+
 void setup() {
-  // put your setup code here, to run once:
   DMXSerial.init(DMXReceiver);
+
+  pinMode(LED, OUTPUT);
+  pinMode(lampOn, OUTPUT);
+  pinMode(setDMXBtn, INPUT_PULLUP);
+
+  pinMode(endStopL, INPUT_PULLUP);
+  pinMode(endStopR, INPUT_PULLUP);
 }
 
+//------------------------------------
+// Run loop until heatdeath of universe
+//------------------------------------
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  //------------------------------------
+  // Handle mirror reflector
+  //------------------------------------
+  int16_t mirrorSpeed = (int16_t)DMXSerial.read(dmxAddr + miOffset) - (int16_t)127;
+
+  mirror.setSpeed(abs(mirrorSpeed));
+  if(mirrorSpeed > 0)
+  {
+    // turn mirror in the one direction
+    mirror.forward();
+  } 
+  else
+  {
+    // other direction
+    mirror.backward();
+  }
+
+  //------------------------------------
+  // Handle azimut
+  //------------------------------------
+  uint8_t azimutSpeed = DMXSerial.read(dmxAddr + azOffset);
+
+  azimut.setSpeed(azimutSpeed);
+  if(digitalRead(endStopR) == 0 and azimut.getDirection() == L298N::BACKWARD)
+  {
+    // we hit the endstop, reverse motor
+    azimut.forward();
+  }
+  if(digitalRead(endStopL) == 0 and azimut.getDirection() == L298N::FORWARD)
+  {
+    // hit the other endstop, reverse again!
+    azimut.backward();
+  }
+
+  //------------------------------------
+  // Handle shutter
+  //------------------------------------
+
+  // update everything every 20ms only, should be fine
+  delay(20);
 }
